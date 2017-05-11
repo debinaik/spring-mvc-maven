b01359dd5bde9100c9f182a6faae11f2550c74a3
Naik <595954@cognizant.com>
2017-05-11 13:39:49 +0530

    First commit

---
+++ src/main/java/com/javatpoint/HelloController.java
@@ -0,0 +1,15 @@
+package com.javatpoint;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HelloController {
+
+	@RequestMapping("/hello")
+	public ModelAndView mymethod(){
+		//return new ModelAndView("hellopage","msg","Hello First Spring");
+		return new ModelAndView("/WEB-INF/jsp/hellopage.html");
+	}
+}
